a8476820cfa6e1e4b530908e74089d43c7f6a199
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-10-13 23:57:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,15 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
-       :patriarch =>{
-         :name => "Lord Montague", 
-         :age => "53"
-       },
-       :matriach => {
-         :name => "Lady Montague",
-         :age => "54"
-       },
-     },
+     :montague => {},
      :capulet => {}
    }
 end
