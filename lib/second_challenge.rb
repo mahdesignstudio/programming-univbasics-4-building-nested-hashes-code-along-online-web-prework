86db10de16d23a2fd51849991d1ca9b07a81b394
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-10-14 00:06:44 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,13 +2,13 @@ def second_challenge
   epic_tragedy = {
     :montague => {
        :patriarch =>{},
-       :matriach => {},
+       :matriarch => {},
        :hero => {},
        :hero_friends =>[ ]
       }, 
     :capulet => {
      :patriarch =>{},
-     :matriach => {},
+     :matriarch => {},
      :heroine => {},
      :heroine_friends =>[ ]
     }
